9a40a0371b8de4f4c40b7e23a07ce4fa5b07e7ce
akshay verma <akshay1kr1verma.av@gmail.com>
2026-05-07 16:10:35 +0530

    config server

---
+++ src/test/java/com/akshay/centralizedconfig/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.centralizedconfig.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
